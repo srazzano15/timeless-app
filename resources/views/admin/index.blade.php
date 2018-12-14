c81c7acd9bfa544d73ad5332bca46f9f684f64c5
srazzano15 <40343937+srazzano15@users.noreply.github.com>
2018-12-13 21:45:28 -0700

    further decorating of admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,2 +1,53 @@
 @extends('layouts.admin')
 
+@section('content')
+
+ <!-- Header -->
+ <header class="w3-container" style="padding-top:22px">
+        <h5><b><i class="fa fa-dashboard"></i> My Dashboard</b></h5>
+      </header>
+
+      <div class="w3-row-padding w3-margin-bottom">
+        <div class="w3-quarter">
+          <div class="w3-container admin_stats w3-padding-16">
+            <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
+            <div class="w3-right">
+              <h3>52</h3>
+            </div>
+            <div class="w3-clear"></div>
+            <h4>Messages</h4>
+          </div>
+        </div>
+        <div class="w3-quarter">
+          <div class="w3-container admin_stats w3-padding-16">
+            <div class="w3-left"><i class="fa fa-eye w3-xxxlarge"></i></div>
+            <div class="w3-right">
+              <h3>99</h3>
+            </div>
+            <div class="w3-clear"></div>
+            <h4>Views</h4>
+          </div>
+        </div>
+        <div class="w3-quarter">
+          <div class="w3-container admin_stats w3-padding-16">
+            <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
+            <div class="w3-right">
+              <h3>23</h3>
+            </div>
+            <div class="w3-clear"></div>
+            <h4>Shares</h4>
+          </div>
+        </div>
+        <div class="w3-quarter">
+          <div class="w3-container admin_stats w3-padding-16">
+            <div class="w3-left"><i class="fa fa-users w3-xxxlarge"></i></div>
+            <div class="w3-right">
+              <h3>50</h3>
+            </div>
+            <div class="w3-clear"></div>
+            <h4>Users</h4>
+          </div>
+        </div>
+      </div>
+
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,45 @@
-<h1>Its working</h1>
+@extends('layouts.admin')
+
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <h1>Users</h1>
+            </div>
+            <div class="col-3 offset-6">
+                <a href="admin/create"><input style="border-color: var(--yellow);" class="btn" type="button" value="Add User"></a>
+            </div>
+        </div>
+    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if ($users)
+
+
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->is_active == 1 ? 'Active' : 'Inactive' }}</td>
+                    <td>{{ $user->created_at->diffForHumans() }}</td>
+
+                </tr>
+            @endforeach
+
+            @endif
+        </tbody>
+    </table>
+
+@endsection
